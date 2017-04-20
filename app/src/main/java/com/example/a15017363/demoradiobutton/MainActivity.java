26a42cc377c6d464e32ca06016f629e06e027a78
15017363 <15017363@myrp.edu.sg>
2017-04-20 12:42:10 +0800

    Updated

--- app/src/main/java/com/example/a15017363/demoradiobutton/MainActivity.java
+++ app/src/main/java/com/example/a15017363/demoradiobutton/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.a15017363.demoradiobutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+//
+//    Button btn;
+//    RadioButton RadiobtnDo;
+//    RadioButton RadiobtnRe;
+//    RadioButton RadiobtnMi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn =(Button)findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Get the RadioGroup object
+                RadioGroup rg = (RadioGroup)findViewById(R.id.radiogroup);
+
+                //Get the id of the selected radio button in the RadioGroup
+                int selectedButtonid = rg.getCheckedRadioButtonId();
+
+                //Get the radio button object from the Id we have gotten above
+                RadioButton rb = (RadioButton)findViewById(selectedButtonid);
+
+                //Show a toast that display the text on the selected radio button
+                Toast.makeText(getBaseContext(), rb.getText(),
+                        Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+
+
     }
 }
